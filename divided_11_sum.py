072e830a14ae14e6e8aa5a7abd15c062788acda7
JamieWei <mycoffeeyoa@gmail.com>
2016-11-03 19:49:23 +0800

    divided by 11 & 13_for_seperated sum

--- divided_11_sum.py
+++ divided_11_sum.py
@@ -1,7 +1,14 @@
-isum = 0
-for i in range(1,101):
-    if i % 11 == 0 or i % 13 == 0:
-        print i
-        isum = isum + i
+sum11 = 0
+sum13 = 0
 
-print isum
+for i in range(1,101):
+    if i % 11 == 0 :
+        print i
+        sum11 = sum11 + i
+
+    elif i % 13 == 0:
+        print i 
+        sum13 = sum13 + i
+
+print sum11
+print sum13
