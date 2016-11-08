e9b8cbacc12c8f40b5872a479acaa00d40056350
JamieWei <mycoffeeyoa@gmail.com>
2016-11-08 20:13:29 +0800

    check range and type of month & year

--- input_practice.py
+++ input_practice.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-Year = input('Year: ')
-if 1 <= Year <= 10000:
-    Year_int = int(Year)
-else:
-    print "Please Enter A Number Between 1 to 10000"
-  
-Month = input('Month:')
-if 1 <= Month <= 12:
-    Month_int = int(Month)
-else:
-    print "Please Enter A Number Between 1 to 12"
+
+
+while True:
+    try:
+        Year = int(input('Year: '))
+    
+        if 1 <= Year <= 10000:
+            break
+        else:
+            print("Please Enter A Number Between 1 to 10000")
+    except:
+        print("Please Enter A Number")
+
+
+while True:
+    try:
+        Month = int(input('Month:'))
+        
+        if 1 <= Month <= 12:
+            break
+        else:
+            print ("Please Enter A Number Between 1 to 12")
+    except:
+        print("Please Enter A Number")
    
 
 calendar = []
@@ -19,5 +32,5 @@ def Fcalendar(x,y):
     calendar.append(y)
     return calendar
 
-Ocalendar = Fcalendar(Year_int, Month_int)
+Ocalendar = Fcalendar(Year, Month)
 print(Ocalendar)
